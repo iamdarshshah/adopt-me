1f209a22e5feaaf8d99a973bb5b081f38d53dead
iamdarshshah <imdarshshah@gmail.com>
2020-02-09 01:18:00 +0530

    useState, custom hooks, update ESLint, bring in petfinder client

--- App.js
+++ App.js
@@ -1,40 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    // return React.createElement("div", {
-    //     id: "something-important"
-    // }, [
-    //     React.createElement("h1", {}, "Adopt Me!"),
-    //     React.createElement(Pet, {
-    //         name: "Luna",
-    //         animal: "Dog",
-    //         breed: "Havanese"
-    //     }),
-    //     React.createElement(Pet, {
-    //         name: "Pepper",
-    //         animal: "Bird",
-    //         breed: "Cockatiel"
-    //     }),
-    //     React.createElement(Pet, {
-    //         name: "Doink",
-    //         animal: "Cat",
-    //         breed: "Mixed"
-    //     })
-    // ]);
-    return(
-        <div>
-            <h1 id="something-important">Adopt Me!</h1>
-            <Pet name="Luna" animal="Dog" breed="Havenese" />
-            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-            <Pet name="Doink" animal="Cat" breed="Mixed" />
-        </div>
-
-    );
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 ReactDOM.render(
-    React.createElement(App), //Composite Component- component you and i have created.
-    document.getElementById("root")
+  React.createElement(App), //Composite Component- component you and i have created.
+  document.getElementById("root")
 );
